2761f56c95f78c1c3281bef2645b773366de97ac
Anzh <anzhrai@gmail.com>
2021-01-07 02:05:10 +0530

    Test Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+From dotenv import load_dotenv
+load_dotenv()
+import os
+
+app = Flask(_name_)
+
+@app.route("/")
+def index():
+    name = request.args.get('name', None)
+
+    if not name:
+        return "Not Found"
+
+    return f"Data you send {name}"
+
+if _name_ == "_main_":
+    app.run()
