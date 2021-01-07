16c707fc7e19fe5f7a1d82838a3898db87025b6c
Anzh <anzhrai@gmail.com>
2021-01-07 15:08:04 +0530

    CORS Added

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS
 load_dotenv()
 import os
 from API.test import x
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
